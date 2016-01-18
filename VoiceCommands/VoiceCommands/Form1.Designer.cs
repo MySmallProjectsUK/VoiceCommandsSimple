b0edfe1f42bbb3bf9c0900ac95e231ba646489fa
MySmallProjectsUK <online@MySmallProjects.com>
2016-01-18 21:41:23 +0000

    Initial

---
+++ VoiceCommands/VoiceCommands/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace VoiceCommands
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblLastComand = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatusListening = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(712, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "123";
+            // 
+            // lblLastComand
+            // 
+            this.lblLastComand.AutoSize = true;
+            this.lblLastComand.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastComand.Location = new System.Drawing.Point(153, 61);
+            this.lblLastComand.Name = "lblLastComand";
+            this.lblLastComand.Size = new System.Drawing.Size(60, 24);
+            this.lblLastComand.TabIndex = 2;
+            this.lblLastComand.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Command heard:";
+            // 
+            // lblStatusListening
+            // 
+            this.lblStatusListening.AutoSize = true;
+            this.lblStatusListening.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusListening.Location = new System.Drawing.Point(7, -2);
+            this.lblStatusListening.Name = "lblStatusListening";
+            this.lblStatusListening.Size = new System.Drawing.Size(40, 24);
+            this.lblStatusListening.TabIndex = 3;
+            this.lblStatusListening.Text = "Idle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 100);
+            this.Controls.Add(this.lblStatusListening);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblLastComand);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblLastComand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatusListening;
+    }
+}
+
---
+++ VoiceCommands/VoiceCommands/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace VoiceCommands
+{
+    public partial class Form1 : Form
+    {
+        private bool IsFormClosing = false;
+
+        private string StartListeningText = "Jarvis";
+        private string StopListeningText = "Cancel";
+        private TimeSpan StopListeningTimeout = new TimeSpan(0,0, 5);
+        protected DateTime ListeningStarted = new DateTime(1950, 1, 1);
+        //protected bool Listening = false;
+        public bool Listening { get {
+                if (DateTime.Now.Subtract(ListeningStarted) > StopListeningTimeout)
+                {
+                    return false;
+                }
+                else
+                    return true;
+            } }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            // Create a new SpeechRecognitionEngine instance.
+            //SpeechRecognizer recognizer = new SpeechRecognizer();
+            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGrammar();
+            StartRecognition();
+             
+
+        }
+
+        private void LoadGrammar() {
+
+
+            // Start Stop Listening            
+            Choices c_StartStop = new Choices();
+            c_StartStop.Add(new string[] { StartListeningText, StopListeningText });
+            GrammarBuilder gb_StartStop = new GrammarBuilder();
+            gb_StartStop.Append(c_StartStop);
+            Grammar g_StartStop = new Grammar(gb_StartStop);
+
+
+            // Create a simple grammar that recognizes "red", "green", or "blue".
+            Choices colors = new Choices();
+            colors.Add(new string[] { "red", "green", "blue", "white", "exit" , "Light On" , "Light Off" });
+            // Create a GrammarBuilder object and append the Choices object.
+            GrammarBuilder gb = new GrammarBuilder();            
+            gb.Append(colors);
+            // Create the Grammar instance and load it into the speech recognition engine.
+            Grammar g_Comamnds = new Grammar(gb);
+                        
+
+
+            // numbers
+            Choices ch_Numbers = new Choices();
+            for(int i = 1; i<= 30; i++)
+                ch_Numbers.Add(i.ToString());
+            //ch_Numbers.Add("1");
+            //ch_Numbers.Add("2");
+            //ch_Numbers.Add("3");
+            //ch_Numbers.Add("4"); // Technically Add(new string[] { "4" });
+            //ch_Numbers.Add("30");
+            //ch_Numbers.Add("13");
+            GrammarBuilder gb_WhatIsXplusY = new GrammarBuilder();
+            gb_WhatIsXplusY.Append("What is");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            gb_WhatIsXplusY.Append("plus");
+            gb_WhatIsXplusY.Append(ch_Numbers);
+            Grammar g_WhatIsXplusY = new Grammar(gb_WhatIsXplusY);
+
+
+            // Build a Grammar object from the XML grammar.
+            string fname = Application.StartupPath +"\\MediaMenuGrammar.grxml";
+            Grammar g_mediaMusic = new Grammar(fname);
+            
+
+
+            recognizer.LoadGrammarAsync(g_StartStop);
+            recognizer.LoadGrammarAsync(g_Comamnds);
+            recognizer.LoadGrammarAsync(g_WhatIsXplusY);
+            recognizer.LoadGrammarAsync(g_mediaMusic);
+
+
+        }
+        private void StartedListening() {
+            //chkListening.Checked = Listening;
+            ListeningStarted = DateTime.Now;
+            //chkListening.Checked = Listening;
+            //textBox1.BackColor = Color.Green;
+            lblStatusListening.BackColor = Color.Green;
+            lblStatusListening.ForeColor = Color.White;
+            lblStatusListening.Text = "Listening...";
+        }         
+        private void StoppedListening() {
+            //chkListening.Checked = Listening;
+            ListeningStarted = DateTime.Now.Subtract(new TimeSpan(0, 0, 30));
+            textBox1.Text = "";
+            //textBox1.BackColor = Color.White;
+            lblStatusListening.BackColor = Color.White;
+            lblStatusListening.ForeColor = Color.Black;
+            lblStatusListening.Text = "Idle";
+        }
+
+        private void StartRecognition() {
+            recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(recognizer_SpeechDetected);
+            recognizer.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(recognizer_SpeechRecognitionRejected);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(recognizer_RecognizeCompleted);
+
+
+            Thread t1 = new Thread(delegate ()
+            {
+                recognizer.SetInputToDefaultAudioDevice();
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            });
+            t1.Start();
+        }
+
+
+        private void recognizer_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            textBox1.Text = "Recognizing voice command...";
+        }
+
+        // Create a simple handler for the SpeechRecognized event.
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            float confidence = e.Result.Confidence;
+            
+            textBox1.Text = "confidence: "  + confidence + ", Recognised:" + e.Result.Text;
+            if (confidence < 0.9)
+                return;
+
+            if (e.Result.Text == StopListeningText)
+            {
+                //ListeningStarted = DateTime.Now.Subtract(new TimeSpan(0, 0, 30));
+                //textBox1.Text = "";
+                StoppedListening();
+            }
+            if (e.Result.Text == StartListeningText)
+            {
+                StartedListening();
+                //ListeningStarted = DateTime.Now;                
+                //chkListening.Checked = Listening;                
+               
+
+
+                // keep a thread going to update the UI
+                Thread t1 = new Thread(delegate ()
+                {
+                    while (Listening)
+                    {
+                        Application.DoEvents();
+                    }
+
+                    if(!IsFormClosing)
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        //chkListening.Checked = Listening;
+                        //textBox1.Text = "";
+                        StoppedListening();
+                    });
+
+                });
+                t1.Start();
+
+                return;
+            }
+
+            // if not listening, do not do anything
+            if (!Listening)
+                return;
+
+
+            lblLastComand.Text = e.Result.Text;
+
+            if (e.Result.Text == "Exit")
+            {
+                recognizer.Dispose();
+                Application.Exit();
+            }
+            if (e.Result.Text == "red")
+            {
+                textBox1.BackColor = Color.Red;
+            }
+            else if (e.Result.Text == "white")
+            {
+                textBox1.BackColor = Color.Wheat;
+            }
+            else if (e.Result.Text == "green")
+            {
+                textBox1.BackColor = Color.Green;
+            }
+
+            else if (e.Result.Text == "Light On")
+            {
+                string cmd = Application.StartupPath + "\\commands\\Plug1-On.vbs";
+                System.Diagnostics.Process.Start(cmd);
+
+            }
+            else if (e.Result.Text == "Light Off")
+            {
+                string cmd = Application.StartupPath + "\\commands\\Plug1-Off.vbs";
+                System.Diagnostics.Process.Start(cmd);
+            }
+
+
+        }
+
+        private void recognizer_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            textBox1.Text = "Failure.";
+        }
+
+        private void recognizer_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            recognizer.RecognizeAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IsFormClosing = true;
+        }
+    }
+}
